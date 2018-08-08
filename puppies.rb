b75471a73ca7a362db5c4b032480d0db54c428f8
Taylor Otey <taylorotey5@gmail.com>
2018-08-08 15:03:21 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -1,6 +1,13 @@
 # 1. Write a method called no_puppies that takes in a number of puppies as an argument. If the number of puppies is greater than zero then this method should return "No more puppies!" otherwise it should return "Well done, Rachel!"
 
-
+def no_puppies(num_puppies)
+  if num_puppies > 0 
+    puts "No more puppies!"
+  elsif num_puppies
+    puts "Well done, Rachel!"
+  end
+end
+  
 
 # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
 
